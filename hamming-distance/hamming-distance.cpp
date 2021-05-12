bd03aad6abfc93818abc1c9f21aa649330df9741
Tanmay Vyas <70322519+Tanmay000009@users.noreply.github.com>
2021-05-12 17:32:52 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (70.47%) - LeetHub

---
+++ hamming-distance/hamming-distance.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        x = x^y;
+        int cnt=0;
+        while (x>0) {
+            
+            if (x&1 == 1) {
+                cnt++;
+            }
+            x = x>>1;
+        }
+    return cnt;
+    }
+};
